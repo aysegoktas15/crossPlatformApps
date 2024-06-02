cb231caca70b5c777c06aeffe22ddfd9e16ad94d
Ayşe <aysegoktas727@gmail.com>
2024-06-02 16:46:29 +0300

    240502

--- Contacts/Contacts.Maui/obj/Debug/net8.0-ios/iossimulator-x64/Contacts.Maui.AssemblyInfo.cs
+++ Contacts/Contacts.Maui/obj/Debug/net8.0-ios/iossimulator-x64/Contacts.Maui.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Contacts.Maui")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+06c442589de8b51dec3b717a9d54364f4fe8815a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+c7a9b884ba3e06ef080a456e473fcbabb0db0f94")]
 [assembly: System.Reflection.AssemblyProductAttribute("Contacts.Maui")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Contacts.Maui")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
