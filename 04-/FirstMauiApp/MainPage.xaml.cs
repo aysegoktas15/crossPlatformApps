ab906ae83880a75d46b9f1e6f8e53475560e90ff
Ayşe <aysegoktas727@gmail.com>
2024-06-29 16:29:22 +0300

    play song

---
+++ 04-/FirstMauiApp/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using FirstMauiApp.ViewModel;
+using Plugin.Maui.Audio; //Add this
+
+namespace FirstMauiApp
+{
+    public partial class MainPage : ContentPage
+    {
+        //int count = 0;
+
+        MainViewModel mwm = new MainViewModel();
+        private readonly IAudioManager audioManager;
+        public MainPage(IAudioManager audioManager)
+        {
+            InitializeComponent();
+            //
+            BindingContext = mwm;
+            this.audioManager = audioManager;
+        }
+
+        private async void OnPlayClicked(object sender, EventArgs e)
+        {
+
+            using var player = audioManager.CreatePlayer(await FileSystem.OpenAppPackageFileAsync("BeMyLover.mp3"));
+            player.Play();
+
+
+            //count++;
+
+            //if (count == 1)
+            //    CounterBtn.Text = $"Clicked {count} time";
+            //else
+            //    CounterBtn.Text = $"Clicked {count} times";
+
+            //SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+
+}
